8faf2bcc52edc8f76d842af7dd07c372f4a7c022
Kenechukwu Nwobodo <nwobodokenechukwu2@gmail.com>
2020-12-11 10:40:59 -0500

    Added some middleware functionality to jwt such as verifyToke instance

--- client/server/api/auth.js
+++ client/server/api/auth.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const signupModels = require("../models/signupModel");
 const { signupChecks, loginChecks } = require("../authValidation");
 const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-const { request } = require("express");
+const jwt = require("jsonwebtoken");
+const { request, response } = require("express");
+const { getMaxListeners } = require("../models/signupModel");
 
-
-
+//SIGNUP POST ROUTE
 router.post("/signup", async (request, response)=> {
     const { error } = signupChecks(request.body);
     if(error) {
@@ -57,10 +57,60 @@ router.post("/login", async (request, response) => {
         return response.status(400).send("Password is incorrect!")
     }
 
+    //JWT TOKEN
     // const sessionToken = jwt.sign({_id: siteUser.id}, process.env.MY_SECRET_TOKEN)
     // response.header("authentication_id", sessionToken).send(sessionToken)
+    
 
-    response.send("logged in successfully")
+    //JWT ASYNCRONOUS METHOD
+    jwt.sign({_id: siteUser.id}, 'secretkey', { expiresIn: '30s'}, (error, token) => {
+        response.json ({
+            message: 'Logged in successfully',
+            token
+        });
+
+    });
 })
+
+// PROFILE DISPLAY ROUTE
+router.post("/profile", verifyToken, (request, response) => {
+    jwt.verify(request.token, 'secretkey', (error, authData) => {
+       if(error) {
+           response.status(403).send('Forbidden');
+       } else {
+        response.json({
+            message: "User profile info and logged in.",
+            authData
+        });
+       }
+    });
+   
+});
+
+//FORMAT TOKEN
+//AUTHORIZATION: BEARER <ACCESS_TOKEN>
+
+//VERIFY TOKEN
+function verifyToken(request, response, next) {
+    //GET THE HEADER VALUE
+    const bearerHeader = request.headers['authorization'];
+
+    //CHECK IF BEARER IS UNDEFINED
+    if(typeof bearerHeader !== 'undefined') {
+        //SPLIT AT THE SPACE
+        const bearer = bearerHeader.split(' ');
+        //GET TOKEN FROM ARRAY
+        const bearerToken = bearer[1];
+        request.token = bearerToken;
+        // CALL NEXT MIDDLEWARE
+        next();
+
+    } else {
+            // RETURN FORBIDDEN
+            response.status(403).send('Forbidden');
+    }
+}
+
+
 
 module.exports = router;
--- client/server/server.js
+++ client/server/server.js
@@ -4,18 +4,21 @@ const authURLs = require("./api/auth");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { required } = require("@hapi/joi");
+const bodyParser = require("body-parser");
+const port = 3000 || process.env.PORT
 
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true}, () => console.log("Database is connected!"));
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () => console.log("Database is connected!"));
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authURLs);
 
 app.listen(3000, () => {
-    console.log("server is running fine!");
+    console.log(`server is running on port ${port}`);
 })
 
 
